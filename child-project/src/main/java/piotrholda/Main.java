73b7c521150a208cac49c385ea87ebe4665e5a54
Piotrek <pholda@gmail.com>
2019-10-26 11:14:11 +0200

    Initial commit

---
+++ child-project/src/main/java/piotrholda/Main.java
@@ -0,0 +1,8 @@
+package piotrholda;
+
+public class Main {
+
+    public static void main(String... args) {
+Any any = new Any();
+    }
+}
---
+++ child-project2/src/main/java/piotrholda/Any.java
@@ -0,0 +1,4 @@
+package piotrholda;
+
+public class Any {
+}
